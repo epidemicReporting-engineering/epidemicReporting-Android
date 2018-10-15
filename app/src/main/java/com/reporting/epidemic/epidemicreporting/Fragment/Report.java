89dc7e1734d505877f995cc793082c292f8e3043
Alex-BlackMamba <alex_decimal@126.com>
2018-10-15 21:57:43 +0800

    change view
    change view

--- app/src/main/java/com/reporting/epidemic/epidemicreporting/Fragment/Report.java
+++ app/src/main/java/com/reporting/epidemic/epidemicreporting/Fragment/Report.java
@@ -1,19 +1,35 @@
 package com.reporting.epidemic.epidemicreporting.Fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.reporting.epidemic.epidemicreporting.R;
+import com.zhihu.matisse.Matisse;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Report extends Fragment {
+public class Report extends Fragment implements View.OnClickListener {
 
+    @BindView(value = R.id.new_report)
+    FloatingActionButton mNewReportButton;
+
+    List<Uri> mSelected;
 
     public Report() {
         // Required empty public constructor
@@ -24,7 +40,26 @@ public class Report extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+        ButterKnife.bind(this, view);
+        mNewReportButton.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.new_report:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 201 && resultCode == RESULT_OK) {
+            mSelected = Matisse.obtainResult(data);
+            Log.d("Matisse", "mSelected: " + mSelected);
+        }
+    }
 }
